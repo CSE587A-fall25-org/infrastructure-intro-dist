bc07e48ee03d93e95372b124b7119436a891f32e
Michael Brent <brent@macbook-pro-94.wusm-wifi.wucon.wustl.edu>
2023-08-07 17:13:48 -0500

    Final pre-dist

--- infrastructure_intro/test_assignment.py
+++ infrastructure_intro/test_assignment.py
@@ -1,5 +1,7 @@
-from python_intro.assignment import hello_world
+from gradescope_utils.autograder_utils.decorators import weight
+from infrastructure_intro.assignment import hello_world
 
+@weight(3)
 def test_hello_world():
     """Test the hello world function"""
     assert hello_world() == "hello world"
