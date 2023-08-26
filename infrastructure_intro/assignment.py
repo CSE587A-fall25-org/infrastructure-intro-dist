d207f23aacda10d15e3a5afb6d1795f783f87c6f
chase mateusiak <chase.mateusiak@gmail.com>
2023-08-26 18:10:59 -0500

    removing snip code

--- infrastructure_intro/assignment.py
+++ infrastructure_intro/assignment.py
@@ -4,5 +4,5 @@ def hello_world():
 	A simple function that returns the string "hello world"
 	"""
     # TODO store the string "hello world" in the variable hello_world_str
-	hello_world_str = "hello world"
+	raise NotImplementedError('write your code here')
 	return hello_world_str
